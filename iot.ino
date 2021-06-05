bb1d9f92a29f2048eeabf99e4b4bb174d88891ab
Thiago Sanches <tsigwt@gmail.com>
2021-06-05 17:00:42 -0300

    Update iot.ino

--- iot.ino
+++ iot.ino
@@ -1,46 +1,81 @@
 #include <Wire.h>
-#include "rgb_lcd.h"
 #include <math.h>
-#define LED 2 //connect LED to digital pin2
-const int B = 4275;               // B value of the thermistor
-const int R0 = 100000;            // R0 = 100k
-const int pinTempSensor = A0;     // Grove - Temperature Sensor connect to A0
+#include "rgb_lcd.h"
+
+#define LED 2
+#define BUZZER 7
+#define EOL '\n' // \n on Windows or \r on Linux
+#define BAUD 9600
 rgb_lcd lcd;
 
+const int B = 4275; // B value of the thermistor
+const int R0 = 100000; // R0 = 100k
+const int pinTempSensor = A0; // Grove - Temperature Sensor connect to A0
 const int colorR = 150;
 const int colorG = 250;
 const int colorB = 0;
 
-void setup() 
+void setup()
 {
-    // set up the LCD's number of columns and rows:
-    lcd.begin(16, 2);
-    pinMode(LED, OUTPUT);
-    
-    lcd.setRGB(colorR, colorG, colorB);
-    
-    // Print a message to the LCD.
-    lcd.print("#FORABOLSONARO");
-    delay(1500);
+  lcd.begin(16, 2);
+  pinMode(LED, OUTPUT);
+  lcd.setRGB(colorR, colorG, colorB);
+
+  Serial.begin(BAUD);
+  lcd.print("#FORABOLSONARO");
+  delay(1500);
 }
 
-void loop() 
+void blinkLed()
 {
-    digitalWrite(LED, HIGH);
-    delay(150);
-    digitalWrite(LED, LOW);
-    delay(150);
+  digitalWrite(LED, HIGH);
+  delay(150);
+  digitalWrite(LED, LOW);
+  delay(150);
+}
 
-    int a = analogRead(pinTempSensor);
-   float R = 1023.0/a-1.0;
-    R = R0*R;
+void beepBuzzer()
+{
+  digitalWrite(BUZZER, HIGH);
+  delay(500);
+  digitalWrite(BUZZER, LOW);
+}
 
-    float temperature = 1.0/(log(R/R0)/B+1/298.15)-273.15;
-    lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print("Temperature:");
-    lcd.setCursor(0, 1);
-    // print the number of seconds since reset:
-    lcd.print(temperature);
-    delay(500);
+void loop()
+{
+  int a = analogRead(pinTempSensor);
+  float R = 1023.0 / a - 1.0;
+  R = R0 * R;
+  float temperature = 1.0 / (log(R / R0) / B + 1 / 298.15) - 273.15;
+  delay(500);
+
+  if (Serial.available() > 0)
+  {
+    char input = Serial.read();
+    switch (input)
+    {
+      case 'T': //temperature
+        Serial.println(temperature);
+        lcd.clear();
+        lcd.setCursor(0, 0);
+        lcd.print((String)temperature + " Celsius");
+        break;
+      case 'B': //blink
+        blinkLed();
+        beepBuzzer();
+        break;
+      case '#':
+        String message = "";
+        input = Serial.read();
+        while (input != EOL)
+        {
+          message += input;
+          input = Serial.read();
+        }
+        lcd.clear();
+        lcd.setCursor(0, 0);
+        lcd.print(message);
+        break;
+    }
+  }
 }
