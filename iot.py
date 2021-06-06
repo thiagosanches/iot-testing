413be7261dcfd661ac2a1638e8422c1f5f5f9e6e
Thiago Sanches <tsigwt@gmail.com>
2021-06-06 14:08:34 -0300

    Delete iot.py

--- iot.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-import serial
-import time
-if __name__ == '__main__':
-    ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
-    ser.flush()
-    ser.write(b"T!\n")
-    ser.write(b"B!\n")
-    ser.write(b"#OI\n")
