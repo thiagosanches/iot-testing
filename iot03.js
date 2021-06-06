a28b7b6cafa091436c2f7dd3949200d1fb3e3424
Thiago Sanches <tsigwt@gmail.com>
2021-06-06 14:18:20 -0300

    Update iot03.js

--- iot03.js
+++ iot03.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser');
 const app = express()
-const port = 3000
+const port = 5000
 const SerialPort = require('serialport')
 const port = new SerialPort('/dev/ttyACM0', { baudRate: 9600 })
 
