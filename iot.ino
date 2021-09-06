27c1461345d14821c19634f18f01c0ad8d4e5c1c
Thiago Sanches <tsigwt@gmail.com>
2021-09-06 17:00:44 -0300

    Update iot.ino

--- iot.ino
+++ iot.ino
@@ -10,9 +10,10 @@
 #define SEPARATOR ':'
 rgb_lcd lcd;
 
-const int B = 4275; // B value of the thermistor
-const int R0 = 100000; // R0 = 100k
-const int pinTempSensor = A0; // Grove - Temperature Sensor connect to A0
+const int B = 4275; // B value of the thermistor.
+const int R0 = 100000; // R0 = 100k.
+const int pinTempSensor = A0; // Grove - Temperature Sensor connect to A0.
+const int pinAdc = A1; //Grove - sound sensor.
 int lcdBackgroundColor[] = {255, 255, 0};
 
 void setup()
@@ -59,6 +60,55 @@ float getTemperature()
   return (1.0 / (log(R / R0) / B + 1 / 298.15) - 273.15);
 }
 
+long getSoundSensorValue()
+{
+  long soundValue = analogRead(pinAdc);
+  lcd.clear();
+  lcd.print(soundValue);
+  lcd.print(" dB");
+  delay(500);
+}
+
+void setLCDBackgroundColor(char input)
+{
+  int currentSeparator = 0;
+  String value = "";
+  while (input != EOL)
+  {
+    input = Serial.read();
+    value += input;
+
+    if (input == SEPARATOR)
+    {
+      lcdBackgroundColor[currentSeparator] = (int)value.toInt();
+      value = "";
+      currentSeparator++;
+    }
+  }
+  lcdBackgroundColor[2] = (int)value.toInt();
+  setupBacklightColor(lcdBackgroundColor[0],
+                      lcdBackgroundColor[1],
+                      lcdBackgroundColor[2]);
+}
+
+void setMessage(char input)
+{
+  lcd.clear();
+  input = Serial.read();
+
+  for (int i = 0; input != EOL; i++)
+  {
+    if (i == MAX_DISPLAY_COLUMN)
+    {
+      lcd.print('-');
+      lcd.setCursor(0, 1);
+    }
+    lcd.print(input);
+    delay(35);
+    input = Serial.read();
+  }
+}
+
 void loop()
 {
   delay(500);
@@ -79,43 +129,15 @@ void loop()
         beepBuzzer(35);
         break;
       case '#':
-        lcd.clear();
-        input = Serial.read();
-
-        for (int i = 0; input != EOL; i++)
-        {
-          if (i == MAX_DISPLAY_COLUMN)
-          {
-            lcd.print('-');
-            lcd.setCursor(0, 1);
-          }
-          lcd.print(input);
-          delay(35);
-          beepBuzzer(35);
-
-          input = Serial.read();
-        }
-        
+        setMessage(input);
         break;
       case 'C':
-        int currentSeparator = 0;
-        String value = "";
-        while (input != EOL)
-        {
-          input = Serial.read();
-          value += input;
-
-          if (input == SEPARATOR)
-          {
-            lcdBackgroundColor[currentSeparator] = (int)value.toInt();
-            value = "";
-            currentSeparator++;
-          }
-        }
-        lcdBackgroundColor[2] = (int)value.toInt();
-        setupBacklightColor(lcdBackgroundColor[0],
-                            lcdBackgroundColor[1],
-                            lcdBackgroundColor[2]);
+        setLCDBackgroundColor(input);
+        break;
+      case '0':
+        getSoundSensorValue();
+        break;
+      default:
         break;
     }
   }
