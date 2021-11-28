06975defd382987db71f94557434042e0ac1f33d
Thiago Sanches <tsigwt@gmail.com>
2021-11-28 08:55:20 -0300

    Update read.py

--- read.py
+++ read.py
@@ -6,20 +6,14 @@ from prometheus_client import start_http_server, Summary, Gauge
 
 IOT_REQUEST_TIME = Summary('iot_request_processing_seconds', 'Time spent processing request')
 IOT_TEMPERATURE = Gauge('iot_temperature', 'Temperature in celsius')
-IOT_HUMIDITY = Gauge('iot_humidity', 'Humidity in percentage')
 
-# Decorate function with metric.
 @IOT_REQUEST_TIME.time()
 def process_request():
     ser = serial.Serial('/dev/ttyACM0', 9600)
-    time.sleep(2)
-    
-    ser.write('T')
-    IOT_TEMPERATURE.set(float(ser.readline().strip()))
-
-    ser.write('H')
-    IOT_HUMIDITY.set(float(ser.readline().strip()))
-
+    time.sleep(5)
+    ser.write(bytes('T', "UTF-8"))
+    data = float(ser.readline().strip())
+    IOT_TEMPERATURE.set(data)
     ser.close()
 
 if __name__ == '__main__':
