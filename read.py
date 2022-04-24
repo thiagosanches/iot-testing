4f5e943260d4e503c77f3be105a1f08ff268a922
Thiago Sanches <tsigwt@gmail.com>
2022-04-24 10:47:14 -0300

    Update read.py

--- read.py
+++ read.py
@@ -9,11 +9,13 @@ IOT_TEMPERATURE = Gauge('iot_temperature', 'Temperature in celsius')
 
 @IOT_REQUEST_TIME.time()
 def process_request():
-    ser = serial.Serial('/dev/ttyACM0', 9600)
-    time.sleep(5)
+    ser = serial.Serial('/dev/ttyACM0', 115200)
+    ser.flushInput()
     ser.write(bytes('T', "UTF-8"))
-    data = float(ser.readline().strip())
+    time.sleep(2)
+    data = float(ser.read(5).strip()) # reading 5 bytes like: XX.XX = 5 bytes
     IOT_TEMPERATURE.set(data)
+    ser.flushOutput()
     ser.close()
 
 if __name__ == '__main__':
